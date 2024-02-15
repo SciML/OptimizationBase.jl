89b13230ab0f68ef5eba05acf0871074ca2ad81b
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2024-02-15 17:11:35 -0500

    Setup package

--- src/OptimizationBase.jl
+++ src/OptimizationBase.jl
@@ -1,5 +1,27 @@
 module OptimizationBase
 
-# Write your package code here.
+using DocStringExtensions
+using Reexport
+@reexport using SciMLBase, ADTypes
+
+if !isdefined(Base, :get_extension)
+    using Requires
+end
+
+using Logging, ProgressLogging, ConsoleProgressMonitor, TerminalLoggers, LoggingExtras
+using ArrayInterface, Base.Iterators, SparseArrays, LinearAlgebra
+using Pkg
+
+import SciMLBase: OptimizationProblem,
+    OptimizationFunction, ObjSense,
+    MaxSense, MinSense, OptimizationStats
+export ObjSense, MaxSense, MinSense
+
+include("function.jl")
+include("adtypes.jl")
+include("cache.jl")
+
+export solve, OptimizationCache
+
 
 end
